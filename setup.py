92ccb5a325170cc2520dc07720b50b5b60d3f692
MaShisongLab <64065760+MaShisongLab@users.noreply.github.com>
2023-02-19 01:23:53 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-    name='TestPackage',
+    name='pytest',
     version='0.1',
     license='BSD-3-Clause',
     author='MaShisongLab',
-    py_modules = ['TestPackage'],
-    url = 'https://github.com/MaShisongLab/py_test',
+    py_modules = ['pytest'],
+    url = 'https://github.com/MaShisongLab/pytest',
     keywords='TestPackage',
     install_requires=['numpy','pandas'],
 )
