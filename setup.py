e497e7f60e7e517d0ca33a55d339c7483fef0f20
MaShisongLab <64065760+MaShisongLab@users.noreply.github.com>
2023-02-19 01:12:02 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     license='BSD-3-Clause',
     author='MaShisongLab',
     py_modules = ['TestPackage'],
-    url = 'https://github.com/MaShisongLab/t',
+    url = 'https://github.com/MaShisongLab/py_test',
     keywords='TestPackage',
     install_requires=['numpy','pandas'],
 )
