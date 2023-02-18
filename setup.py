5fc0869d6732286804c45cbdaf5c8f2b56e7e961
MaShisongLab <64065760+MaShisongLab@users.noreply.github.com>
2023-02-19 01:11:02 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='TestPackage',
+    version='0.1',
+    license='BSD-3-Clause',
+    author='MaShisongLab',
+    py_modules = ['TestPackage'],
+    url = 'https://github.com/MaShisongLab/t',
+    keywords='TestPackage',
+    install_requires=['numpy','pandas'],
+)
+
