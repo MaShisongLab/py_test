9ab9f5964eda936ca8bf13c2420927203a8657dc
MaShisongLab <64065760+MaShisongLab@users.noreply.github.com>
2024-05-09 19:20:57 +0800

    Create setup.py

---
+++ python/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pytesting',
+    version='0.1',
+    license='BSD-3-Clause',
+    author='MaShisongLab',
+    py_modules = ['pytesting'],
+    url = 'https://github.com/MaShisongLab/pytesting',
+    keywords='pytesting',
+    install_requires=['numpy','pandas'],
+)
